7e9ec43c1b0388490ddbaf104b7ecb915d8e149d
MaksL777 <lukanenkomaks427@gmail.com>
2026-07-23 15:11:06 +0200

    Change error code from '4' to '404' on NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,10 @@
 import { Link } from "react-router-dom";
-import Icon from "../../components/Icon/Icon";
 import styles from "./NotFoundPage.module.css";
 
 export default function NotFoundPage() {
   return (
     <div className={styles.wrapper}>
-      <span className={styles.code}>
-        4<Icon name="black-map" size={56} className={styles.codeIcon} />4
-      </span>
+      <span className={styles.code}>404</span>
       <h1 className={styles.title}>Page not found</h1>
       <p className={styles.text}>
         The page you are looking for doesn&apos;t exist or was moved.
